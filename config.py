2d30e372d422623868c59883b9b32e4dcb354ae2
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-10-09 18:17:54 +0900

    Refactor config and Vanilla_SSM for variable-length input
    Updated config.py to support variable-length input sequences with zero-padding and dynamic feature extraction. Refactored Vanilla_SSM to use new config structure, modular linear layers, and added discretization logic for recurrent output. These changes improve flexibility and compatibility with datasets containing variable-length features.

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import io
 import zipfile
-import re
 import requests
 import torch
 
@@ -8,24 +7,51 @@ import torch
 class Config:
   def __init__(self, download=True):
     self.mode = False # true if you want sequential output
-    self.episode_length = 10
 
-    self.A_channels = 64
-    self.B_channels = 64
-    self.C_channels = 64
-    self.D_channels= 64
-
-    self.bias = True
-
-    # dataset
     if download: UCI_HAR_download()
     x, y = UCI_HAR_read(
       x_path='UCI_HAR_Dataset/UCI HAR Dataset/train/X_train.txt',
       y_path='UCI_HAR_Dataset/UCI HAR Dataset/train/y_train.txt',
-    ) # UCI_HAR_read
+    )  # UCI_HAR_read
     self.x, self.y = list(map(str_to_tensor, x)), list(map(lambda val: torch.tensor(float(val)), y))
+    print('str_to_tensor is applied to self.x\nint_to_hv is applied to self.y')
+    self.episode_length = len(self.x[0])
+    self.max_var_len = get_max_x_len(self.x)
+    self.x = map(lambda val: zero_padding(val, self.max_var_len), self.x)
+    self.x = list(self.x)
+    self.output_dim = self.y[0].shape[0]
+
+    # hippo init
+    in_features, hid_features, out_features = 64, 64, 10
+    self.delta = {
+      "k": self.episode_length,
+      "in_features": in_features,
+      "hid_features": hid_features,
+      "out_features": out_features
+    } # self.delta
+    self.A = {
+      "in_features": in_features,
+      "out_features": hid_features,
+    } # self.A
+    self.B = {
+      "in_features": in_features,
+      "out_features": hid_features,
+    }  # self.B
+    self.C = {
+      "in_features": in_features,
+      "out_features": out_features,
+    }  # self.C
+    self.D = {
+      "in_features": in_features,
+      "out_features": out_features,
+    }  # self.D
+    self.bias = True
+
+    self.dummy = torch.zeros_like(self.x[0])
   # __init__
 # Config
+
+linear_apply = lambda config: nn.Linear(in_features=config.in_features, out_features=config.out_features)
 
 def UCI_HAR_download(url='https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip'):
   response = requests.get(url)
@@ -51,13 +77,33 @@ def UCI_HAR_read(x_path, y_path):
 
 def str_to_tensor(val):
   val = val.strip()
-  val = re.split(r'\s+', val)
-  val = [float(feature) for feature in val]
-  return torch.tensor(val)
+  val = val.split('  ')
+  val = [torch.tensor([float(feature) for feature in seq.split(' ')]) for seq in val]
+  return val
 # str_to_tensor
+
+def zero_padding(sequence, max_length):
+  return_sequence = list()
+  for feature in sequence:
+    len_feature = feature.shape[0]
+    if len_feature < max_length:
+      zeros = torch.zeros(max_length)
+      for idx, item in enumerate(feature): zeros[idx] = item
+      return_sequence.append(zeros)
+    else:
+      return_sequence.append(feature)
+  return return_sequence
+# _zero_padding
+
+def get_max_x_len(dataset):
+  sequence = dataset[0]
+  length_list = list()
+  for feature in sequence: length_list.append(feature.shape[0])
+  return max(length_list)
+# get_max_x_len
 
 if __name__ == "__main__":
   config = Config(download=False)
-  for feature, label in zip(config.x, config.y):
-    print(f'feature: {feature.shape} label: {label.item()}')
+  print(f'max_var_len: {config.max_var_len}')
+  for feature in config.x[0]: print(f'feature: {feature.shape[0]}', end=' ')
 # if __name__ == "__main__":
--- model/Vanilla_SSM.py
+++ model/Vanilla_SSM.py
@@ -1,17 +1,33 @@
 import torch
 from torch import nn
 
+
 class Vanilla_SSM(nn.Module):
   def __init__(self, config):
     super(Vanilla_SSM, self).__init__()
+    self.config = config
     self.mode = self.config.mode
-    self.A = nn.Linear(in_features=config.A_channels, out_features=config.A_channels, bias=config.bias)
-    self.B = nn.Linear(in_features=config.B_channels, out_features=config.B_channels, bias=config.bias)
-    self.C = nn.Linear(in_features=config.C_channels, out_features=config.C_channels, bias=config.bias)
-    self.D = nn.Linear(in_features=config.D_channels, out_features=config.D_channels, bias=config.bias)
+
+    self.A = linear_apply(self.config.A)
+    self.B = linear_apply(self.config.B)
+    self.C = linear_apply(self.config.C)
+    self.D = linear_apply(self.config.D)
+
+    self.deltas = nn.ModuleList()
+    self.deltas.append(nn.Linear(in_features=config.delta.in_features, out_features=config.delta.hid_features))
+    for i in range(1, self.config.episode_length - 1):
+      self.deltas.append(nn.Linear(in_features=config.delta.hid_features, out_features=config.delta.hid_features))
+    self.deltas.append(nn.Linear(in_features=config.delta.hid_features, out_features=config.delta.out_features))
   # __init__
 
-  def process(self, h, x): return self.A(h) + self.B(x)
+  def process(self, h, x, k):
+    a = self.A(h)
+    a_d = self.deltas[k](a)
+    b_k = self.B(x)
+    delta_k_b_k = self.deltas[k](b_k)
+    b_d = (a_d ** -1) * (a_d - I) * delta_k_b_k
+    return torch.exp(a_d) + b_d
+  # process
   def measurement(self, h, x): return self.C(h) + self.D(x)
 
   def step(self, x, h):
@@ -45,10 +61,23 @@ class Vanilla_SSM(nn.Module):
       outputs.append(output.item())
     return torch.stack(outputs)
   # recurrent_sequential_output
+
+  def recurrent_no_sequential_output_with_discretization(self, sequence):
+    iv_state = self.config.dummy  # later
+    episode_length, outputs = len(sequence), list()
+    state_buffer = self.step(x=sequence[0], h=iv_state)
+    for x_k, index in enumerate(sequence[1:]):
+      if index != episode_length - 1: state_buffer = self.step(x=feature, h=state_buffer)
+      output, state_buffer = self.step(x=x_k, h=state_buffer)
+      outputs.append(output.item())
+    return torch.stack(outputs)
+  # discretization
 # Vanilla_SSM
 
+
 if __name__ == "__main__":
-  from config import Config
+  from config import Config, linear_apply
+
   ssm_config = Config()
   ssm = Vanilla_SSM(ssm_config)
 # if __name__ == "__main__":
